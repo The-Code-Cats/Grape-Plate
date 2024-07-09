47a2cc82870e083d05bb416a611e123e811ce49e
akravt1274 <akravt1274@gmail.com>
2024-07-08 23:01:51 -0500

    Updated api to get source url and redirect to the source page; clean up code by organizing functions

---
+++ Assets/js/display.js
@@ -0,0 +1,78 @@
+function displayDishPairingForWine(winePairings) {
+
+    // Remove the content for below elements when calling this function
+    const searchResultsDiv = $('#search-results');
+    searchResultsDiv.empty();
+
+    const receipesDiv = $('#recipes');
+    receipesDiv.empty();
+
+    if (winePairings.status != 'failure') { 
+        
+        const desc = $('<p>').addClass('p-2 mb-0 fw-bold').text(winePairings.text);
+        const rec = $('<p>').addClass('p-2 mb-0 fw-bold').css('color', 'green' ).text('Click the link below to access top rated recipes that are easy to make and sure to please:');
+        
+        const div = $('<div>');
+        for (let i = 0; i < winePairings.pairings.length; i++) {
+            let button = $('<button>').addClass('dish btn btn-link').text(winePairings.pairings[i]);
+            div.append(button); 
+        }        
+        $('#search-results').append(desc, rec, div);  
+    }
+    else {
+        const msg = $('<p>').addClass('p-2 mb-0').css('color', 'red').text(winePairings.message);
+        $('#search-results').append(msg); 
+        console.log('Wine not recognized');
+    }    
+}
+
+function displayRecipes(recipes) {
+    
+    // Remove the content for below elements when calling this function
+    const receipesDiv = $('#recipes');
+    receipesDiv.empty();
+
+    if (recipes.status != 'failure') {  
+        for (let i = 0; i < recipes.results.length; i++) {
+            const recipeID = recipes.results[i].id;            
+            const title = $('<button>').attr({'id':recipeID}).addClass('recipe btn btn-link').text(recipes.results[i].title);
+            const img = $('<img>').attr({'src':`${recipes.results[i].image}`, 'alt':'recipe-image'});
+            const divImg = $('<div>');
+            divImg.append(img);
+            const divReceipe = $('<div>').addClass('float-start p-2');
+            divReceipe.append(title, divImg);
+            $('#recipes').append(divReceipe);           
+        }      
+    }
+    else {
+        const msg = $('<p>').addClass('p-2 mb-0').css('color', 'red').text(recipes.message);
+        $('#search-results').append(msg); 
+        console.log('Could not find a recipe');
+    }    
+}
+
+function displaySourceURL(sourceURL) {    
+    location.assign(sourceURL);
+}
+
+function displayPairedWine(pairedWines) {
+
+    // Remove the content for below elements when calling this function
+    const searchResultsDiv = $('#search-results');
+    searchResultsDiv.empty();
+
+    const receipesDiv = $('#recipes');
+    receipesDiv.empty();
+        
+    if (pairedWines.status != 'failure') {        
+            
+        const pairingText = $('<p>').addClass('p-2 mb-0 fw-bold').text(pairedWines.pairingText);
+        $('#search-results').append(pairingText);
+      
+    }
+    else {
+        const msg = $('<p>').addClass('p-2 mb-0').css('color', 'red').text(pairedWines.message);
+        $('#search-results').append(msg); 
+        console.log('Could not find a wine pairing for meet');
+    }    
+}
--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,11 +1,7 @@
-const APIKey = '8af27eda82f94fbaab427393bacc26d8';
-let dishes = []; // used to build query params for the Recipes API
+const APIKey = 'a957835674014562ab42b1ac05dec254';
 
-function searchWineHandler (event) {
-    
+function searchWineHandler (event) {    
     event.preventDefault(); 
-
-    // Variable to hold search input
     const searchTerm = $('#searchWine').val().trim(); 
 
     if (searchTerm === "") {
@@ -13,20 +9,15 @@ function searchWineHandler (event) {
         $('#errorWine').text('Please enter wine').css( 'color', 'red' );
     }
     else {        
-        getDishPairingForWine(searchTerm);
+        getDishPairingForWine(searchTerm);        
         
-        // Clear search input
-        $('#searchWine').val('');             
-        // Clear error message
-        $('#errorWine').text(''); 
+        $('#searchWine').val(''); // Clear search input        
+        $('#errorWine').text(''); // Clear error message 
     }
 }
 
-function searchFoodHandler (event) {
-   
+function searchFoodHandler (event) {   
     event.preventDefault(); 
-
-    // Variable to hold search input
     const searchTerm = $('#searchFood').val().trim(); 
 
     if (searchTerm === "") {
@@ -36,99 +27,21 @@ function searchFoodHandler (event) {
     else {
         getWinePairing(searchTerm);
         
-        // Clear search input
-        $('#searchFood').val('');
-        // Clear error message
-        $('#errorFood').text('');
+        $('#searchWine').val(''); // Clear search input        
+        $('#errorWine').text(''); // Clear error message
     }
 }
 
-function searchRecipeHandler (event) {
-    
-    event.preventDefault(); 
-
-    const recipeID = event.target.id; // used to get recipeInfo by ID
-    getRecipeInfo(recipeID);
+function searchRecipeHandler (event) {    
+    event.preventDefault();
+    const dish = event.target.innerText;
+    searchRecipes(dish);
 }
 
-function displayRecipeSource(sourceURL) {
-    // location.replace(redirectUrl);
-}
-
-function displayDishPairingForWine(winePairings) {
-
-    // Remove the content for below elements when calling this function
-    const searchResultsDiv = $('#search-results');
-    searchResultsDiv.empty();
-
-    const receipesDiv = $('#recipes');
-    receipesDiv.empty();
-
-    dishes = winePairings.pairings;    
-    
-
-    if (winePairings.status != 'failure') { 
-        
-        const desc = $('<p>').addClass('p-2 mb-0 fw-bold').text(winePairings.text);
-        $('#search-results').append(desc);   
-        
-        searchRecipesByIngredients(dishes);
-    }
-    else {
-        const msg = $('<p>').addClass('p-2 mb-0').css('color', 'red').text(winePairings.message);
-        $('#search-results').append(msg); 
-        console.log('Wine not recognized');
-    }
-    
-}
-
-function displayRecipes(recipes) {
-            
-    if (recipes) {  
-        const question = $('<p>').addClass('float-start p-2 mb-0 fw-bold').css('color', 'green' ).text('Our top 10 rated recipes that are easy to make and sure to please:');
-        $('#search-results').append(question);
-
-        for (let i = 0; i < recipes.length; i++) {
-            const recipeID = recipes[i].id;
-            getRecipeInfo(recipeID);
-            
-            // console.log('sourceURL 2',sourceURL);
-            // const title = $('<button>').attr({'id':recipeID}, {'src': sourceURL}).addClass('recipe btn btn-link').text(recipes[i].title);
-            const title = $('<button>').attr({'id':recipeID}).addClass('recipe btn btn-link').text(recipes[i].title);
-            const img = $('<img>').attr({'src':`${recipes[i].image}`, 'alt':'recipe-image'});
-            const divImg = $('<div>');
-            divImg.append(img);
-            const divReceipe = $('<div>').addClass('float-start p-2');
-            divReceipe.append(title, divImg);
-            $('#recipes').append(divReceipe);           
-        }      
-    }
-    else {
-        console.log('Wine not recognized');
-    }
-    
-}
-
-function displayPairedWine(pairedWines) {
-
-    // Remove the content for below elements when calling this function
-    const searchResultsDiv = $('#search-results');
-    searchResultsDiv.empty();
-
-    const receipesDiv = $('#recipes');
-    receipesDiv.empty();
-        
-    if (pairedWines.status != 'failure') {        
-            
-        const pairingText = $('<p>').addClass('p-2 mb-0 fw-bold').text(pairedWines.pairingText);
-        $('#search-results').append(pairingText);
-      
-    }
-    else {
-        const msg = $('<p>').addClass('p-2 mb-0').css('color', 'red').text(pairedWines.message);
-        $('#search-results').append(msg); 
-        console.log('Could not find a wine pairing for meet');
-    }    
+function searchRecipeInfoHandler (event) {
+    event.preventDefault();
+    const recipeID = event.target.id;
+    getRecipeSourceURL(recipeID);
 }
 
 $(document).ready(function () {
@@ -136,6 +49,7 @@ $(document).ready(function () {
     // Call event handler functions when search input is submitted
     $('#search-wine').on('submit', searchWineHandler);  // Enter key
     $('#search-food').on('submit', searchFoodHandler);  // Enter key
-    $(document).on('click', '.recipe', searchRecipeHandler);  // Click on recipe
+    $(document).on('click', '.dish', searchRecipeHandler);  // Click on a dish
+    $(document).on('click', '.recipe', searchRecipeInfoHandler);  // Click on a recipe
 
 });
