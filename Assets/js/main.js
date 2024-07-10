5709d2a0f47fb81d9f87cc76aab29475e591250c
Wrandy1 <cjyoung0420@gmail.com>
2024-07-09 21:10:02 -0500

    change

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,4 +1,6 @@
 const APIKey = 'a957835674014562ab42b1ac05dec254';
+let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
 
 function searchWineHandler (event) {    
     event.preventDefault(); 
@@ -62,4 +64,68 @@ $(document).ready(function () {
     $(document).on('click', '.recipe', searchRecipeInfoHandler);  // Click on a recipe link
     $(document).on('click', '.wine', wineHandler);  // Click on a wine link
 
+
+
+
 });
+
+$('#modalSubmitButton').on('click', handleModalSubmit);  // Enter key
+let usernameInputEl = $('#userName');
+let emailInputEl = $('#emailInput');
+
+
+
+
+function handleModalSubmit(){
+    // console.log("here");
+    // console.log(emailInputEl.val());
+    // console.log(usernameInputEl.val());
+    // console.log( typeof(usernameInputEl.val()));
+    if (usernameInputEl.val() !== "" && emailInputEl.val() !== ""){ // THIS ISN'T WORKING
+        // create a user object
+        const userObject = {
+            userName : usernameInputEl.val(),
+            email : emailInputEl.val()
+        };
+
+        // console.log(userObject);
+
+        localStorage.setItem('currentUser',JSON.stringify(userObject));
+        document.getElementById("myModal").classList.remove("is-active");
+        $('#userNameElement').html(userObject.userName);
+        // closeModal();
+
+        // save to local storage
+        // refresh page
+    }
+else{
+        alert('Invaild username or email. Please try again.');
+
+
+    }
+
+}
+
+function closeModal() {
+    console.log("is this working?");
+    document.getElementById("myModal").classList.remove("is-active");
+  }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get modal and open button elements
+    var modal = document.getElementById('myModal');
+    var openModalButton = document.getElementById('openModalButton');
+
+    // Add click event listener to open button
+    openModalButton.addEventListener('click', function () {
+      modal.classList.add('is-active'); // Add 'is-active' class to show modal
+    });
+
+    // Add click event listener to close modal when clicking outside modal or on close button
+    modal.addEventListener('click', function (event) {
+      if (event.target === modal || event.target.classList.contains('delete')) {
+        modal.classList.remove('is-active'); // Remove 'is-active' class to hide modal
+      }
+    });
+  });
